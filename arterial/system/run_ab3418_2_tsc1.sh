0ad9775b782d739756b347bc46672a70da779736
John Spring <jspring@berkeley.edu>
2018-08-15 19:44:51 -0700

    new file:   doc/Arterial signal IP addresses.docx new file:   doc/Arterial signal IP addresses.txt new file:   doc/Field_Test_Plan_HNcomments.docx new file:   doc/PATH field computer to PATH server connection.jpg new file:   doc/PATH field computer to PATH server connection.pptx Added directory and some documents pertainint to arterial signal control modified:   system/run_ab3418_2_tsc1.sh Added IP address as an argument

--- arterial/system/run_ab3418_2_tsc1.sh
+++ arterial/system/run_ab3418_2_tsc1.sh
@@ -2,4 +2,7 @@
 
 IPADDR=$1
 DATESTR=`date +%m%d%y_%H%M%S`
-ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.11.93 -o 2011 -P 20 -T 2111 -v" | grep "udp response" >$IPADDR.$DATESTR.txt
+LATESTFILE=/linux2/big/data/SANJOSE_ACRM/$IPADDR.$DATESTR.txt
+ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.131.150 -o 2011 -P 20 -T 2111 -v" | grep "udp response" >$LATESTFILE
+
+echo $LATESTFILE >"/home/sj_i680_capitol/arterial/data/"$IPADDR"_latest.txt"
