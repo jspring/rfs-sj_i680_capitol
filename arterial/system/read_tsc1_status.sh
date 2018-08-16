872b6ed00f9a50bb62e314732b88259a690aa850
John Spring <jspring@berkeley.edu>
2018-08-15 19:39:18 -0700

    new file:   read_all new file:   read_tsc1_status.sh Scripts for only reading TSC1 controllers

---
+++ arterial/system/read_tsc1_status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IPADDR=$1
+DATESTR=`date +%m%d%y_%H%M%S`
+LATESTFILE=/linux2/big/data/SANJOSE_ACRM/$IPADDR.$DATESTR.txt
+ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.131.150 -o 2011 -T 2111 -v" | grep "udp response" >$LATESTFILE
+
+echo $LATESTFILE >"/home/sj_i680_capitol/arterial/data/"$IPADDR"_latest.txt"
