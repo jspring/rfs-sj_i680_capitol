bb0bbbcbf62a5788e986b765b9a5a7f18363a097
John Spring <jspring@berkeley.edu>
2018-09-26 20:20:16 -0700

    modified:   ../../arterial/system/read_tsc1_status.sh Added usage message Tried port 162 with no success modified:   clt_vars.h Changed VAR defs from number to TYPE db_controller_list: moved DB_166_253_101_113_VAR, etc. to top of 	array so they're listed in location order Also db_trig_list

--- arterial/system/read_tsc1_status.sh
+++ arterial/system/read_tsc1_status.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [[ q$1 == 'q' ]]
+then
+	echo "Usage: $0 <remote ip address>"
+	exit 1
+fi
 IPADDR=$1
 DATESTR=`date +%m%d%y_%H%M%S`
 LATESTFILE=/linux2/big/data/SANJOSE_ACRM/$IPADDR.$DATESTR.txt
-ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.131.150 -o 2011 -T 2111 -v" | grep "udp response" >$LATESTFILE
+#ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.131.93 -o 2011 -T 2111 -v" | grep "udp response" >$LATESTFILE
+ssh -p 5571 jspring@localhost "/home/atsc/ab3418/lnx/ab3418comm -a $IPADDR -A 10.192.131.93 -o 162 -v" | grep "udp response"
 
 echo $LATESTFILE >"/home/sj_i680_capitol/arterial/data/"$IPADDR"_latest.txt"
